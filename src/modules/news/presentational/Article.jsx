4dcb1c10312ece3e10aae609ccfd4125de6fb933
Narasimha <narasimha@codebrahma.com>
2017-04-20 11:48:11 +0530

    Code Refactor * Changed some component classes to pure functions

--- src/modules/news/presentational/Article.jsx
+++ src/modules/news/presentational/Article.jsx
@@ -1,17 +1,11 @@
 import React, {Component} from 'react';
 
-class Article extends Component {
-	render () {
-		var article = this.props.article;
-
-		return (
-			<div className="article">
-				<h3>{article.title}</h3>
-				<img src={article.urlToImage} alt={article.title}/>
-				<p>{article.description.slice(0, 150)}</p>
-			</div>
-		)
-	}
+export default function Article({article}) {
+	return (
+		<div className="article">
+			<h3>{article.title}</h3>
+			<img src={article.urlToImage} alt={article.title}/>
+			<p>{article.description.slice(0, 150)}</p>
+		</div>
+	)
 }
-
-export default Article;
